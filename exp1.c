589ecd6e6c9b6d1756022e5fb21aa49014b1c5d8
Lee <1821015428lqy@gmail.com>
2023-04-22 20:01:43 +0800

    add comment to exp1.c

--- exp1.c
+++ exp1.c
@@ -5,16 +5,16 @@
 typedef struct pcb
 {
     char name[20];
-    int time_left;
-    int time_require;
-    int priority;
-    char status;
-    struct pcb *next;
+    int time_left;//进程剩余运行时间
+    int time_require;//进程需要运行的时间
+    int priority;//优先级
+    char status;//状态
+    struct pcb *next;//指向下一个PCB
     /* data */
 }PCB;
 int priority_num[ProcessNum]={1,3,2,5,4};
 int time_require_num[ProcessNum]={4,8,10,3,11};
-PCB *head,*PCB1,*PCB2,*PCB3,*PCB4,*PCB5;
+PCB *head;
 void init();
 void run();
 void sort();
@@ -30,6 +30,7 @@ void init(){
     int n=ProcessNum;
     int i;
 	PCB *p;
+    //创建链表
 	head=(PCB*)malloc(sizeof(PCB));
 	p=head;
 	for(i=0;i<n;i++){
@@ -39,7 +40,7 @@ void init(){
 		p=t;
 	}
 	p->next=NULL;
-    
+    //遍历链表，给name赋值
     PCB *head1=head; 
     i=1;
     while(head1->next)
@@ -48,6 +49,7 @@ void init(){
     	head1=head1->next;
     	i++;
 	}
+    //遍历链表，给priority赋值
 	i=0;
 	head1=head;
 	while(head1->next){
@@ -55,24 +57,23 @@ void init(){
 		head1=head1->next;
 		i++;
 	}
-	
+	//遍历链表，给time_require赋值
 	i=0;
 	head1=head;
-
 	while(head1->next){
 		head1->next->time_require=head1->next->time_left=time_require_num[i];
 		head1=head1->next;
 		i++;
 	}
-	
+	//给status赋值
 	for(head1=head;head1->next;head1=head1->next){
 		head1->next->status='R';
 	}
-	sort();
+	sort();//按priority从大到小排序
 
 	
 }
-void sort(){
+void sort(){//冒泡排序，按priority从大到小排序
 	PCB *head1;
 	PCB *head2;
 	for(head1=head;head1->next;head1=head1->next)
@@ -85,20 +86,20 @@ void sort(){
 					head2->next=t;
 				}
 		}
-	for (head1 = head; head1->next; head1 = head1->next)
-	{
-		printf("%d ", head1->next->priority);
+	// for (head1 = head; head1->next; head1 = head1->next)
+	// {
+	// 	printf("%d ", head1->next->priority);
 		
-	}printf("\n");
-	for (head1 = head; head1->next; head1 = head1->next)
-	{
-		printf("%d ", head1->next->time_left);
+	// }printf("\n");
+	// for (head1 = head; head1->next; head1 = head1->next)
+	// {
+	// 	printf("%d ", head1->next->time_left);
 		
-	}printf("\n");
+	// }printf("\n");
 }
-void run(){
+void run(){//运行处理器调度程序
 	PCB *head1;
-	while(head->next)
+	while(head->next)//当链表不为空时运行处理器调度程序
 	{
 		
 		head1=head;
@@ -123,12 +124,12 @@ void run(){
 	}
 	printf("运行结束");
 }
-int run_once(PCB* p){
+int run_once(PCB* p){//运行某个PCB
 	int end=0;
-	if (p->next->priority != 0)
+	if (p->next->priority != 0)//运行某个PCB时，priority减1，最小值为0
 		p->next->priority--;
 	printf("当前运行进程:%s\n", p->next->name);
-	if (--p->next->time_left == 0)
+	if (--p->next->time_left == 0)//时间为0则表示运行结束，status设为E
 	{
 		p->next->status = 'E';
 		end=1;
@@ -137,11 +138,11 @@ int run_once(PCB* p){
 	
 	return end;
 }
-void print_queue(){
+void print_queue(){//打印队列
 	PCB *head1;
 	if(head->next)
 	printf("进程队列:");
-	for(head1=head;head1->next;head1=head1->next)
+	for(head1=head;head1->next;head1=head1->next)//遍历链表打印
 	{	
 		if(head1->next->time_left)
 		{
@@ -154,7 +155,7 @@ void print_queue(){
 	}
 	
 }
-void del(PCB *p){
+void del(PCB *p){//如果进程剩余时间为0，就将PCB从队列中删除
 	PCB *t=p->next;
 	p->next=t->next;
 	free(t);
